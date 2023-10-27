094cc5c616f5e6465c1b85eadd4a271751ec3dc6
Júlia Sendra <juliasendra98@gmail.com>
2023-10-27 17:33:29 +0200

    player and first scoop created

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const player = new Player();
+const scoop = new Scoop();
+
+document.addEventListener("keydown", (e) => {
+    switch (e.code) {
+        case "ArrowLeft":
+            player.moveLeft();
+            break;
+        case "ArrowRight":
+            player.moveRight();
+            break;
+    }
+});
