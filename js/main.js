e60de47e23b4168cc3e9c551b00fae74613b5e2c
Júlia Sendra <juliasendra98@gmail.com>
2023-10-28 14:22:22 +0200

    adding the scoop in the cone

--- js/main.js
+++ js/main.js
@@ -16,6 +16,19 @@ function fallDown() {
     setInterval(
         function() {
             scoop.moveDown();
+            if (scoop.positionY < 0 - scoop.height){
+                scoop.scoopElm.remove(); 
+            }
+            if (
+            player.positionX < scoop.positionX + scoop.width &&
+            player.positionX + player.width > scoop.positionX &&
+            player.positionY < scoop.positionY + scoop.height &&
+            player.positionY + player.height > scoop.positionY
+            ){
+                scoop.scoopElm.remove();
+                let newHeight = player.height * 2;
+                player.playerElm.style.height = newHeight + "vh";
+            }
         }
     ,100);
 }
