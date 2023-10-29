c3036f94ee866f5a3df2565e527d5766a801da78
Júlia Sendra <juliasendra98@gmail.com>
2023-10-29 23:34:18 +0100

    1st icons added - UI

--- js/player.js
+++ js/player.js
@@ -1,7 +1,7 @@
 class Player {
     constructor() {
         this.width = 10;
-        this.height = 15;
+        this.height = 10;
         this.positionX = 50 - (this.width / 2);
         this.positionY = 0;
 
@@ -10,6 +10,8 @@ class Player {
         this.playerElm.style.height = this.height + "vh";
         this.playerElm.style.left = this.positionX + "vw";
         this.playerElm.style.bottom = this.positionY + "vh";
+
+        this.scoopsInCone = 0;
     }
     moveLeft() {
         this.positionX--;
@@ -20,12 +22,27 @@ class Player {
         this.playerElm.style.left = this.positionX + "vw";
     }
     grow() {
-        this.height += 15;
+        this.height += 5;
         this.playerElm.style.height = this.height + 'vh';
+        this.scoopsInCone++;
+        switch(this.scoopsInCone) {
+            case 1:
+                this.playerElm.style.backgroundImage = "url(../images/1-pink-scoop.png)";
+                break;
+            case 2:
+                this.playerElm.style.backgroundImage = "url(../images/2-pink-scoops.png)";
+                break;
+            case 3:
+                this.playerElm.style.backgroundImage = "url(../images/3-pink-scoops.png)";
+                break;
+
+        }
     }
     reset() {
-        this.height = 15;
-        this.playerElm.style.height = 15 + 'vh';    
+        this.height = 10;
+        this.playerElm.style.height = 10 + 'vh';    
+        this.scoopsInCone = 0;
+        this.playerElm.style.backgroundImage = "url(../images/cone.png)";
     }
 
 }
--- js/scoop.js
+++ js/scoop.js
@@ -1,7 +1,7 @@
 class Scoop {
     constructor(scoopId) {
         this.width = 10;
-        this.height = 15;
+        this.height = 10;
         this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
         this.positionY = 100 - (this.height/2);
         this.fallSpeed = 2;
@@ -15,6 +15,7 @@ class Scoop {
         this.scoopElm.style.height = this.height + "vh";
         this.scoopElm.style.left = this.positionX + "vw";
         this.scoopElm.style.bottom = this.positionY + "vh";
+        this.scoopElm.style.backgroundImage = "url(../images/pink-scoop.png";
     }
 
     moveDown() {
