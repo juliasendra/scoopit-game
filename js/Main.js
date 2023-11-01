5f1dde5d45b535c12fee2e99abf30d486162f32d
Júlia Sendra <juliasendra98@gmail.com>
2023-11-02 00:46:22 +0100

    title image added

--- js/Main.js
+++ js/Main.js
@@ -27,188 +27,188 @@ this.levelElm.id = "level";
 this.levelElm.textContent = "Level: " + level;
 board.appendChild(this.levelElm);
 
-document.getElementById("playButton").addEventListener("click", function() {
+document.getElementById("playButton").addEventListener("click", function () {
     document.getElementById("instructions").style.display = "none";
     document.getElementById("game").style.display = "block";
-  
- 
-  
+    document.getElementById("imageTitle").style.display = "none";
+    
 
-document.addEventListener("keydown", (e) => {
-    switch (e.code) {
-        case "ArrowLeft":
-            player.moveLeft();
-            break;
-        case "ArrowRight":
-            player.moveRight();
-            break;
-    }
-});
 
-function fallDown(scoop) {
-    let myInterval = setInterval(function () {
-        if (scoop.positionY < 0 - scoop.height || this.isGameOver) {
-            scoop.scoopElm.remove();
-            clearInterval(myInterval);
-            return;
+    document.addEventListener("keydown", (e) => {
+        switch (e.code) {
+            case "ArrowLeft":
+                player.moveLeft();
+                break;
+            case "ArrowRight":
+                player.moveRight();
+                break;
         }
+    });
 
-        if (checkCollision(player, scoop)) {
-            scoop.scoopElm.remove();
-            if (scoopsAmount < 3) {
-                scoopsAmount++;
-                player.grow();
-            } else {
-                scoopsAmount = 0;
-                player.reset();
-                score++;
-                this.scoreElm.textContent = "Score: " + score;
-            }
-            bubbleSound.play();
-            clearInterval(myInterval);
-        }
-
-        scoop.moveDown();
-    }, 100);
-}
-
-function fruitFallDown(fruit) {
-    let myInterval = setInterval(function () {
-        if (fruit.positionY < 0 - fruit.height || this.isGameOver) {
-            fruit.fruitElm.remove();
-            clearInterval(myInterval);
-            return;
-        }
-
-        if (checkCollision(player, fruit)) {
-            clearInterval(myInterval);
-            fruit.fruitElm.remove();
-            gameOver();
-            return;
-        }
-        fruit.moveDown();
-    }, 100);
-    fruit.fallInterval = myInterval;
-}
-
-function snowflakeGoUp(snowflake) {
-    let myInterval = setInterval(() => {
-        if (snowflake.positionY > 100 - snowflake.height || this.isGameOver) {
-            snowflake.snowflakeElm.remove();
-            clearInterval(myInterval);
-            return;
-        }
-        
-        for (let i = 0; i < fruitsArr.length; i++) {
-            let fruit = fruitsArr[i];
-            if (checkSnowflakeCollision(fruit, snowflake)) {
-                snowflake.snowflakeElm.remove();
-                fruit.fruitElm.remove();
-                fruitsArr.splice(i, 1);
-                clearInterval(fruit.fallInterval);
+    function fallDown(scoop) {
+        let myInterval = setInterval(function () {
+            if (scoop.positionY < 0 - scoop.height || this.isGameOver) {
+                scoop.scoopElm.remove();
                 clearInterval(myInterval);
-                convertFruitToScoop(fruit);
                 return;
             }
-        }
-        snowflake.moveUp();
-    }, 100);
-}
 
-function convertFruitToScoop(fruit) {
-    fallingScoopCount++;
-    const scoop = new Scoop("scoop" + fallingScoopCount);
-    scoop.setPosition(fruit.positionX, fruit.positionY);
-    scoopsArr.push(scoop);
-    fallDown(scoop);
-}
+            if (checkCollision(player, scoop)) {
+                scoop.scoopElm.remove();
+                if (scoopsAmount < 3) {
+                    scoopsAmount++;
+                    player.grow();
+                } else {
+                    scoopsAmount = 0;
+                    player.reset();
+                    score++;
+                    this.scoreElm.textContent = "Score: " + score;
+                }
+                bubbleSound.play();
+                clearInterval(myInterval);
+            }
 
-function generateScoop() {
-    scoopsInterval = setInterval(() => {
+            scoop.moveDown();
+        }, 100);
+    }
+
+    function fruitFallDown(fruit) {
+        let myInterval = setInterval(function () {
+            if (fruit.positionY < 0 - fruit.height || this.isGameOver) {
+                fruit.fruitElm.remove();
+                clearInterval(myInterval);
+                return;
+            }
+
+            if (checkCollision(player, fruit)) {
+                clearInterval(myInterval);
+                fruit.fruitElm.remove();
+                gameOver();
+                return;
+            }
+            fruit.moveDown();
+        }, 100);
+        fruit.fallInterval = myInterval;
+    }
+
+    function snowflakeGoUp(snowflake) {
+        let myInterval = setInterval(() => {
+            if (snowflake.positionY > 100 - snowflake.height || this.isGameOver) {
+                snowflake.snowflakeElm.remove();
+                clearInterval(myInterval);
+                return;
+            }
+
+            for (let i = 0; i < fruitsArr.length; i++) {
+                let fruit = fruitsArr[i];
+                if (checkSnowflakeCollision(fruit, snowflake)) {
+                    snowflake.snowflakeElm.remove();
+                    fruit.fruitElm.remove();
+                    fruitsArr.splice(i, 1);
+                    clearInterval(fruit.fallInterval);
+                    clearInterval(myInterval);
+                    convertFruitToScoop(fruit);
+                    return;
+                }
+            }
+            snowflake.moveUp();
+        }, 100);
+    }
+
+    function convertFruitToScoop(fruit) {
         fallingScoopCount++;
         const scoop = new Scoop("scoop" + fallingScoopCount);
+        scoop.setPosition(fruit.positionX, fruit.positionY);
         scoopsArr.push(scoop);
         fallDown(scoop);
-    }, 3000);
-}
-
-generateScoop();
-
-function generateFruit() {
-    let count = 0;
-    fruitsInterval = setInterval(() => {
-        count++
-        const fruit = new Fruit("fruit" + count);
-        fruitsArr.push(fruit);
-        fruitFallDown(fruit);
-    }, 800);
-}
-
-generateFruit();
-
-function checkCollision(player, scoop) {
-    return (
-        player.positionX < scoop.positionX + scoop.width &&
-        player.positionX + player.width > scoop.positionX &&
-        player.positionY < scoop.positionY + scoop.height &&
-        player.positionY + player.height > scoop.positionY
-    );
-} 
-
-function checkFruitCollision(player, fruit) {
-    return (
-        player.positionX < fruit.positionX + fruit.width &&
-        player.positionX + player.width > fruit.positionX &&
-        player.positionY < fruit.positionY + fruit.height &&
-        player.positionY + player.height > fruit.positionY
-    );
-}
-
-function checkSnowflakeCollision(fruit, snowflake) {
-    return (
-        snowflake.positionX < fruit.positionX + fruit.width &&
-        snowflake.positionX + snowflake.width > fruit.positionX &&
-        snowflake.positionY < fruit.positionY + fruit.height &&
-        snowflake.positionY + snowflake.height > fruit.positionY
-    );
-}
-
-function gameOver() {
-    this.isGameOver = true;
-
-    clearInterval(scoopsInterval);
-    clearInterval(fruitsInterval);
-
-    player.playerElm.remove();
-    negativeBeeps.play();
-
-    const gameOver = document.getElementById("gameOver");
-    const restartButton = document.getElementById("restartButton");
-
-    gameOver.style.display = "block";
-    
-    restartButton.addEventListener("click", () => {
-        resetGame();
-    });
-}
-
-function resetGame() {
-    gameOverElement.style.display = "none";
-    location.reload();
-}
-
-document.addEventListener("keydown", (e) => {
-    if (e.key === " ") {
-        shootSnowflake();
-        e.preventDefault(); 
     }
-});
 
-function shootSnowflake() {
-    const playerPosition = player.getPlayerPosition();
-    const snowflakeId = "snowflake" + Date.now();
-    const snowflake = new Snowflake(snowflakeId, playerPosition.positionX, playerPosition.positionY);
-    snowflakeGoUp(snowflake)
-}
+    function generateScoop() {
+        scoopsInterval = setInterval(() => {
+            fallingScoopCount++;
+            const scoop = new Scoop("scoop" + fallingScoopCount);
+            scoopsArr.push(scoop);
+            fallDown(scoop);
+        }, 3000);
+    }
+
+    generateScoop();
+
+    function generateFruit() {
+        let count = 0;
+        fruitsInterval = setInterval(() => {
+            count++
+            const fruit = new Fruit("fruit" + count);
+            fruitsArr.push(fruit);
+            fruitFallDown(fruit);
+        }, 800);
+    }
+
+    generateFruit();
+
+    function checkCollision(player, scoop) {
+        return (
+            player.positionX < scoop.positionX + scoop.width &&
+            player.positionX + player.width > scoop.positionX &&
+            player.positionY < scoop.positionY + scoop.height &&
+            player.positionY + player.height > scoop.positionY
+        );
+    }
+
+    function checkFruitCollision(player, fruit) {
+        return (
+            player.positionX < fruit.positionX + fruit.width &&
+            player.positionX + player.width > fruit.positionX &&
+            player.positionY < fruit.positionY + fruit.height &&
+            player.positionY + player.height > fruit.positionY
+        );
+    }
+
+    function checkSnowflakeCollision(fruit, snowflake) {
+        return (
+            snowflake.positionX < fruit.positionX + fruit.width &&
+            snowflake.positionX + snowflake.width > fruit.positionX &&
+            snowflake.positionY < fruit.positionY + fruit.height &&
+            snowflake.positionY + snowflake.height > fruit.positionY
+        );
+    }
+
+    function gameOver() {
+        this.isGameOver = true;
+
+        clearInterval(scoopsInterval);
+        clearInterval(fruitsInterval);
+
+        player.playerElm.remove();
+        negativeBeeps.play();
+
+        const gameOver = document.getElementById("gameOver");
+        const restartButton = document.getElementById("restartButton");
+
+        gameOver.style.display = "block";
+
+        restartButton.addEventListener("click", () => {
+            resetGame();
+        });
+    }
+
+    function resetGame() {
+        gameOverElement.style.display = "none";
+        location.reload();
+    }
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key === " ") {
+            shootSnowflake();
+            e.preventDefault();
+        }
+    });
+
+    function shootSnowflake() {
+        const playerPosition = player.getPlayerPosition();
+        const snowflakeId = "snowflake" + Date.now();
+        const snowflake = new Snowflake(snowflakeId, playerPosition.positionX, playerPosition.positionY);
+        snowflakeGoUp(snowflake)
+    }
 
 });
